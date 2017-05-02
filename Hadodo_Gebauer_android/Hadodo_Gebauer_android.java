eb743854e959e49cced07ec53e6e38dc80133b0c
chainstair <benjamin.gezgin@hotmail.com>
2017-05-02 15:41:54 +0200

    Code fortgesetzt

--- Hadodo_Gebauer_android/Hadodo_Gebauer_android.java
+++ Hadodo_Gebauer_android/Hadodo_Gebauer_android.java
@@ -4,11 +4,13 @@ public static void main (String[]a){
 	System.out.println("Pr0gramm");
 }
 
-<<<<<<< HEAD
 public thisIsANewMethod() { }
 	Ra1nbow.getColoures("BuntesEinhorn");
-=======
 
 
->>>>>>> e8cbc641323b56ab00b7a8267265f500ad048d0c
+public pferdeFlüstern() {
+	for (int i = 0; i < 10; i++) {
+		System.out.println("Wuff \n ups \n ich mein hüyaaa" );
+	}
+}
 }
